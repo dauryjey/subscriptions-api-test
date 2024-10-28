b526d12b5ca4aea944a2784a55a7d432359934c4
dauryjey <dauryjey@gmail.com>
2024-10-27 21:27:12 -0400

    My first endpoint

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello API")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "My first endpoint")
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
